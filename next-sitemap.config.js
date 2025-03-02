035fa7797b8861bf3e460ff7cfe5a0c35cf9d45e
viunow <vini.bracht1@gmail.com>
2025-03-02 00:14:35 -0300

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,11 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://www.viniciusneto.dev',
-  generateRobotsTxt: true, // Altere para true para garantir que o robots.txt seja atualizado
+  generateRobotsTxt: true,
   outDir: './public',
   sitemapSize: 7000,
   exclude: ['/admin', '/api/*', '/sentry-example-page'],
   robotsTxtOptions: {
-    additionalSitemaps: [
-      // Você pode adicionar sitemaps adicionais aqui se necessário
-    ],
     policies: [
       {
         userAgent: '*',
